25852ca4b39e3697f0f4e974285e6a580a7ba0a4
alowce <alicebrandt.c@gmail.com>
2021-05-26 21:48:50 -0300

    Listando tutoriais do usuário

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    Olá, aqui estão seus tutoriais!
                 </div>
+                @foreach (Auth::user()->tutorials as $tutorials)
+                    <div class="px-6 py-3">
+                        Título: {{ $tutorials->title }} 
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
